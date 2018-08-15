89f7ce5fdfae6130053ae7bece67970d33954359
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-15 22:12:30 +0200

    loging pagina added and routes with express

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
 const app = express();
 
 // set up view engine
 app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth', authRoutes);
 
 // create home route
 app.get('/', (req, res) => {
---
+++ routes/auth-routes.js
@@ -0,0 +1,22 @@
+'user strict'
+
+const router = require('express').Router();
+
+// auth login
+router.get('/login', (req, res) => {
+  res.render('login');
+})
+
+// auth logout
+router.get('/logout', (req, res) => {
+  // handle with passport
+  res.send('logging out');
+})
+
+// auth with google
+router.get('/google', (req, res) => {
+  // handle with passport
+  res.send('logging with google');
+})
+
+module.exports = router;
